f8dca75e093a813e5aaaae1be2ad4cbf7f560e00
Ievgen Sein <eseinv@gmail.com>
2019-05-14 23:32:13 +0300

    added relation to new images class

--- app/Product.php
+++ app/Product.php
@@ -29,4 +29,9 @@ class Product extends Model implements AuthenticatableContract, AuthorizableCont
     protected $hidden = [
 
     ];
+
+    public function images()
+    {
+        return $this->hasMany('App\Image', 'productId', 'id');
+    }
 }
